3423bb435ca189be9b7ece579390ab8291a168af
King Rayhan <rayhan095@gmail.com>
2020-03-29 11:19:32 +0600

    more validation rules

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ![](ss/ss1.png)
 ![](ss/ss2.png)
+![](ss/ss3.png)
+![](ss/ss4.png)
+![](ss/ss5.png)
--- lib/src/mixins/ValidaorMixin.dart
+++ lib/src/mixins/ValidaorMixin.dart
@@ -3,12 +3,22 @@ import 'package:string_validator/string_validator.dart'
 
 class ValidatorMixin {
   String emailValidator(String input) {
+    if (input.length == 0) {
+      return "Email is required";
+    }
     if (!isEmail(input)) {
       return "Provide a valide email address";
     }
   }
 
   String passwordValidator(String input) {
+    if (input.length == 0) {
+      return "Password is required";
+    }
+
+    if (input.length > 0 && input.length < 6)
+      return "Give at least 5 characters";
+
     if (!isAlphanumeric(input.trim())) {
       return "Special character is not allowed";
     }
